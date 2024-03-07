23f5a55ddc295612d894f07af639c784e9f7c5a9
Monir <fencemonir@gmail.com>
2024-03-07 19:23:16 +0600

    fixed: button url escaped issue

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c34c5ba95de1e9ccb52f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f7bf98a5c36b54b7497e');
